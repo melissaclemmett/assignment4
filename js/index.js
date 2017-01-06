7e4de83c8a9e86f5257ce6083283c63976f5d42f
Melissa Clemmett <melissaclemmett@gmail.com>
2017-01-06 22:19:45 +1100

    Committing final version of assignment

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
-  $(".sectionlink").click(function(event){
+  // alert("TEST");
+  $(".hide").hide();
+  $(".sectionLink").click(function(event){
     event.preventDefault();
+    alert("Sorry, we're not taking any new submissions right now!");
   });
-  $(".main.blog div section a").click(function){
-
-  }
-
-
+  $(".headerLink").click(function(event){
+    event.preventDefault();
+    alert("Sorry, we're not taking any new submissions right now.");
+  });
+  $(".readMore").click(expandBlogPost1);
+  $(".readLess").click(collapseBlogPost1);
+  $(".learnMore").click(expandAboutRelaxr);
+  $(".categories").click(function(event){
+    event.stopPropagation();
+  })
 });
+
+function expandBlogPost1() {
+  event.preventDefault();
+  $("#showThisOnClick" + ".readLess").removeClass("hide");
+  $("#showThisOnClick").slideDown();
+  $(".readLess").show();
+  $(".readMore").hide();
+}
+
+function collapseBlogPost1() {
+  event.preventDefault();
+  $("#showThisOnClick" + ".readLess").addClass("hide");
+  $("#showThisOnClick").slideUp();
+  $(".readLess").hide();
+  $(".readMore").show();
+}
+
+function expandAboutRelaxr() {
+  event.preventDefault();
+  $("#learnMoreText").slideDown();
+  $(".learnMore").hide();
+}
